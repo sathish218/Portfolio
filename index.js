bf7c2635de576c6f9fba068e63861d51896a42b3
sathish218 <sathishsuganesan@gmail.com>
2024-09-05 14:10:06 +0530

    modified images

---
+++ index.js
@@ -0,0 +1,24 @@
+let mark=50;
+
+if(mark>60)
+{
+    console.log("1st class pass");
+    console.log("A grade");
+}
+else if(mark>45) {
+    console.log("2nd class");
+    console.log("B grade");
+
+}
+else{
+    console.log("fail");
+    console.log("C grade");
+
+}
+//mark 0-"fail"
+//mark 25-"fail"
+//mark 45-"fail"
+//mark 50-"pass"
+//mark 60-"pass"
+//mark 75-"1st class"
+//mark 90-"district first"
